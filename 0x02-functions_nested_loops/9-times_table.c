da4bb225a8c14b7812b32817b2e799dea807f5f5
Amanda Mabunda <amandamabu14@gmail.com>
2023-07-18 13:40:12 -0700

    9-times_table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,32 +7,27 @@
 
 void times_table(void)
 {
-	int x, y, z, u, d;
+	int x, y, z;
 
 	for (x = 0; x <= 9; x++)
 	{
 		for (y = 0; y <= 9; y++)
 		{
 		z = x * y;
+		if (y != 0)
+		{
+		_putchar(',');
+		_putchar(' ');
+		}
 		if (z < 10)
 		{
-		u = u % 10;
-		d = (z - u) / 10;
-		_putchar(44);
-		_putchar(32);
-		_putchar(d + '0');
-		_putchar(u + '0');
+		_putchar(' ');
 		}
 		else
 		{
-		if (y != 0)
-		{
-		_putchar(44);
-		_putchar(32);
-		_putchar(32);
+		_putchar((z / 10) + '0');
 		}
-		_putchar(u + '0');
-		}
+		_putchar((z % 10) + '0');
 		}
 		_putchar('\n');
 	}
