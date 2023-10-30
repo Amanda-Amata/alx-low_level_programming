80771ffb055e082565572c361cdeeb31020d45bc
Amanda Mabunda <amandamabu14@gmail.com>
2023-10-30 10:02:35 -0700

    0-read_textfile

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -16,27 +16,32 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t writefile;
 	char *buffer;
 
-	if (filename == NULL)
-		return (0);
-
-	file_name = open(filename, O_RDONLY);
-	if (file_name == -1)
+	if (filename == NULL || letters == 0)
 		return (0);
 
 	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
 		return (0);
 
+	file_name = open(filename, O_RDONLY);
+	if (file_name == -1)
+	{
+		free(buffer);
+		return (0);
+	}
+
 	readfile = read(file_name, buffer, letters);
 	if (readfile == -1)
 	{
+		free(buffer);
 		close(file_name);
 		return (0);
 	}
-	
+
 	writefile = write(STDOUT_FILENO, buffer, readfile);
 	free(buffer);
-	if (readfile != writefile)
+	close(file_name);
+	if (writefile != readfile)
 		return (0);
-	return (writefile);
+	return (readfile);
 }
