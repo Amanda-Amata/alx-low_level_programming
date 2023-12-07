d2be19c1f1d91ff1cbf2b4aebd1f6ad77c699743
Amanda Mabunda <amandamabu14@gmail.com>
2023-12-07 11:20:47 -0800

    all mendatory tasks

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - adds a new node at the beginning of a list
+ * @head: head of the list
+ * @n: value of the element
+ * Return: adress of new element
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new;
+	dlistint_t *i;
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+	new->prev = NULL;
+	i = *head;
+
+	if (i != NULL)
+	{
+		while (i->prev != NULL)
+			i = i->prev;
+	}
+
+	new->next = i;
+
+	if (i != NULL)
+		i->prev = new;
+
+	*head = new;
+
+	return (new);
+}
---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - returns the nth node of a dlistint_t linked list.
+ * @head: pointer to head
+ * @index: index to search in node from 0
+ * Return:nth node or NULL
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int size;
+	dlistint_t *temp;
+
+	size = 0;
+	if (head == NULL)
+		return (NULL);
+
+	temp = head;
+	while (temp)
+	{
+		if (index == size)
+			return (temp);
+		size++;
+		temp = temp->next;
+	}
+	return (NULL);
+}
