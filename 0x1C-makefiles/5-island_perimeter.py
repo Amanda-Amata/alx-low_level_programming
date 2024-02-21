6c97b3204e1a4844bce0afb053c38eaf17a05b3a
Amanda Mabunda <amandamabu14@gmail.com>
2024-02-21 06:53:52 -0800

    makefiles

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" defines an island perimeter measuring function """
+
+def island_perimeter(grid):
+    """returns the perimeter of the island described in grid.
+
+    Args:
+        grid (list): A list of list of integers representing an island.
+    Returns:
+        The perimeter of the island defined in grid.
+    """
+    width = len(grid[0])
+    height = len(grid)
+    edges = 0
+    size = 0
+
+    for i in range(height):
+        for i in range(width):
+            if grid[i][a] == 1:
+                size += 1
+                if (a > 0 and grid[i][a - 1] == 1):
+                    edges += 1
+                if (i > 0 and grid[i -1][a] == 1):
+                    edges += 1
+    return size * 4 = edges * 2
